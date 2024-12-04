bb6acfd65059662a4f7318d98017651b4693bb40
kirill <kulakov165k@gmail.com>
2024-12-04 18:36:35 +0500

    detail film

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,8 +8,12 @@ export default defineNuxtConfig({
         {
           rel: 'stylesheet',
           href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css'
-        }
-      ],
+        },
+        {
+          rel: 'stylesheet',
+      href: 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css',
+    },
+],
       script: [
         {
           src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js',
---
+++ pages/film/[ID].vue
@@ -0,0 +1,74 @@
+<script setup>
+
+</script>
+<template>
+  <div class="row">
+    <div class="col-md-2">
+      <h2> 4.8 </h2> </div>
+    <div class="col-md-8">
+      <iframe width="750" height="450" src="https://r380101.yandexwebcache.org/dandadan/1.480.1be5ec6d71684fb4.mp4?hash1=c1d49d45b7df0e7acc7ce54f5e1aa6d3&hash2=39a5064f967faf578ae05ebb2e40d8c8"></iframe>
+    </div>
+    <div class="col-md-2">
+      <ul class="rating-stars">
+        <li class="star-item"><a href="" class="star-link" data-bs-toggle="tooltip" title="5"><i class="bi bi-star"></i></a></li>
+        <li class="star-item"><a href="" class="star-link" data-bs-toggle="tooltip" title="5"><i class="bi bi-star"></i></a></li>
+        <li class="star-item"><a href="" class="star-link" data-bs-toggle="tooltip" title="5"><i class="bi bi-star"></i></a></li>
+        <li class="star-item"><a href="" class="star-link" data-bs-toggle="tooltip" title="5"><i class="bi bi-star"></i></a></li>
+        <li class="star-item"><a href="" class="star-link" data-bs-toggle="tooltip" title="5"><i class="bi bi-star"></i></a></li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-12">
+      <h1>Dandadan</h1>
+    </div>
+
+    <div class="col-md-8 fs-5">
+      23 min  Japan  2024  16+  03.12.2024  Action, Fantastic &nbsp
+    </div>
+
+    <div class="col-md-4-fs-4 text-end">
+      <a href="#">Kinopoisk</a>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col">
+      <label for="exampleFormControlTextarea1" class="form-label">Add review</label>
+      <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </div>
+    <div class="col-md-2 d-flex flex-column-reverse">
+      <button class="btn btn-outline-success">send</button>
+    </div>
+  </div>
+  <div class="col-12 fs-4 mt-2">Revies</div>
+  <div class="row">
+    <div class="col">
+      <div class="row">
+        <div class="col-md-10 fw-bold fs-5">kilka</div>
+        <div class="col-md-2">04.12.2024</div>
+      </div>
+      <div class="row">
+        <div class="col fos-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium atque aut commodi dolor eaque enim expedita inventore iure magni modi molestias nostrum, officia quis suscipit vero vitae. Corporis, illum?</div>
+        <div class="col-12"><hr class="border border-black border-2 opacity-50"></div>
+      </div>
+    </div>
+
+
+  </div>
+</template>
+<style scoped>
+.rating-stars {
+  list-style-type: none;
+}
+.star-item {
+  font-size: 2.5rem;
+}
+.star-link {
+  color: #490495;
+}
+.star-link:hover {
+  color: #ff00cd;
+}
+</style>
